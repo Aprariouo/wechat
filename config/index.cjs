c633e7c0eb4863408e13cb8785b75cd2e5cd3beb
Aprariouo <139528848+Aprariouo@users.noreply.github.com>
2023-11-21 23:20:10 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -25,7 +25,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oRR5i6XuskRaz7v4HANjJIcLIY_U',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'ln88yfONLO-uE2WeXc5kGLczlYCkh3CTmlLp0M0PBJc',
+      useTemplateId: '_6fExgliBJ0wDza7XsJ7FhwINyeOWKFUEUAVYayfXh8',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '10-04',
       // 我们在一起已经有xxxx天了的配置
