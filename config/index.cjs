2776e9c73d090f2957a20f976bee122b1dd88779
Aprariouo <139528848+Aprariouo@users.noreply.github.com>
2023-11-24 09:42:04 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -20,23 +20,6 @@ const USER_CONFIG = {
   },
  
   USERS: [
-   /**
-    {
-      // 想要发送的人的名字
-      name: '老婆',
-      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oRR5i6XuskRaz7v4HANjJIcLIY_U',
-      // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'tunec3PeykBQEcYBVBPrLiHepjKED_IVMM0YHBEXTo4',
-      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '10-04',
-      // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2023-11-11' },
-      ],
-    },
-    */
        {
       // 想要发送的人的名字
       name: '自己',
