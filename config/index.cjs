254b80459ad4943903d8c1fcd8113f4ae8f6cc1f
Aprariouo <139528848+Aprariouo@users.noreply.github.com>
2023-11-24 10:49:01 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -29,7 +29,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oRR5i6XuskRaz7v4HANjJIcLIY_U',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '70_hSS4PY2ASHxkynjkCxsvGMQzBMPpGl9I67UWUDaM',
+      useTemplateId: '_Bmdys3L6EYZKk9kyotwsz01eVaoga_1qUDbsrXIRE0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '10-04',
       // 我们在一起已经有xxxx天了的配置
@@ -44,7 +44,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oRR5i6Y1fTAZuuT9HjwHHpN-F0UY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '70_hSS4PY2ASHxkynjkCxsvGMQzBMPpGl9I67UWUDaM',
+      useTemplateId: '_Bmdys3L6EYZKk9kyotwsz01eVaoga_1qUDbsrXIRE0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '10-04',
       // 我们在一起已经有xxxx天了的配置
